71e294accedb38e826854c7eb252ae6b8ea67d4e
jesserme <jesserme@usc.edu>
2021-04-25 19:14:16 -0700

    D2

---
+++ D2.cmd
@@ -0,0 +1,10 @@
+universe = vanilla
+executable = /bin/bash
+getenv = true
+output = output
+error = stderr
+log = log
+arguments = "run.sh"
+transfer_executable = false
+queue
+
---
+++ src/run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python3 emotion_detection.py /home2/jesserme/src/D2/dev.tsv /home2/jesserme/src/D2/train.tsv
