9e2c9b69bde7ebe8de1f785e25bff8c0807dfd1d
jesserme <jesserme@usc.edu>
2021-05-09 18:52:24 -0700

    D3

---
+++ src/D3/D3.cmd
@@ -0,0 +1,11 @@
+universe = vanilla
+executable = /bin/bash
+getenv = true
+output = output
+error = stderr
+log = log
+arguments = "run.sh"
+transfer_executable = false
+request_memory = 2*1024
+queue
+
---
+++ src/D3/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+/home2/jesserme/spanish_emotions/py_3.9/bin/python3 emotion_detection.py /home2/jesserme/src/D3/dev.tsv /home2/jesserme/src/D3/train.tsv > output.txt
+
