5e8834ab4b94e7ddfd04b5026dbff9a3705e7e0d
jesserme <jesserme@usc.edu>
2021-05-30 17:55:03 -0700

    name change

--- src/D4/adaptation/emotion_counts.py
+++ src/D4/adaptation/emotion_counts.py
@@ -5,7 +5,7 @@ import collections
 
 dic = collections.defaultdict(lambda: 0)
 
-with open("train_nor_811.csv", "r") as in_f:
+with open("train.csv", "r") as in_f:
 
     csv_reader = csv.reader(in_f)
 
